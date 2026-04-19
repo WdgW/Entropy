912a24a922fbca08379adf747e69686a32694b61
WdgW <1442474121@qq.com>
2026-04-19 16:17:55 +0800

    fix bug

--- src/entropy/Entropy.kt
+++ src/entropy/Entropy.kt
@@ -12,8 +12,8 @@ import entropy.EntropyContentType as ECT
 
 
 class Entropy : Mod() {
-    val mod: Mods.LoadedMod = Vars.mods.getMod(Entropy::class.java)
-    val contentRoot: Fi = mod.root.child("content")
+    val mod: Mods.LoadedMod by lazy {Vars.mods.getMod(Entropy::class.java)}
+    val contentRoot: Fi by lazy {mod.root.child("content") }
    // val configs
     //var isLoadExamples: Boolean = contentRoot.exists(false) {
      //   // TODO
